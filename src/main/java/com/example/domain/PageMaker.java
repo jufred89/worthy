ca10dd6a57b430619a773bd3bd6e545e901f15b6
jufred89 <jufred89@gmail.com>
2021-10-26 09:25:38 +0900

    worthy project start

---
+++ src/main/java/com/example/domain/PageMaker.java
@@ -0,0 +1,80 @@
+package com.example.domain;
+
+public class PageMaker {
+	private int totalCount;  //전체데이타갯수
+	private int startPage; //시작페이지
+	private int endPage;  //마지막페이지
+	private boolean prev; //이전페이지 버튼
+	private boolean next; //다음페이지 버튼
+	private int displayPageNum = 10;
+	
+	private Criteria cri;
+
+	public void setCri(Criteria cri) {
+		this.cri = cri;
+	}
+
+	public void setTotalCount(int totalCount) {
+		this.totalCount = totalCount;
+		calcData();
+	}
+
+	private void calcData() {
+		endPage = (int) (Math.ceil(cri.getPage() / (double) displayPageNum) * displayPageNum);
+		startPage = (endPage - displayPageNum) + 1;
+		int tempEndPage = (int) (Math.ceil(totalCount / (double) cri.getPerPageNum()));
+		if (endPage > tempEndPage) {
+			endPage = tempEndPage;
+		}
+		prev = startPage == 1 ? false : true;
+		next = endPage * cri.getPerPageNum() >= totalCount ? false : true;
+	}
+
+	public int getStartPage() {
+		return startPage;
+	}
+
+	public void setStartPage(int startPage) {
+		this.startPage = startPage;
+	}
+
+	public int getEndPage() {
+		return endPage;
+	}
+
+	public void setEndPage(int endPage) {
+		this.endPage = endPage;
+	}
+
+	public boolean isPrev() {
+		return prev;
+	}
+
+	public void setPrev(boolean prev) {
+		this.prev = prev;
+	}
+
+	public boolean isNext() {
+		return next;
+	}
+
+	public void setNext(boolean next) {
+		this.next = next;
+	}
+
+	public int getDisplayPageNum() {
+		return displayPageNum;
+	}
+
+	public void setDisplayPageNum(int displayPageNum) {
+		this.displayPageNum = displayPageNum;
+	}
+
+	public int getTotalCount() {
+		return totalCount;
+	}
+
+	public Criteria getCri() {
+		return cri;
+	}
+}
---
+++ src/test/java/com/example/controller/MysqlTest.java
@@ -0,0 +1,21 @@
+package com.example.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.example.mapper.MysqlMapper;
+
+@RunWith(SpringJUnit4ClassRunner.class) //먼저 SpringJUnit4ClassRunner.class import한다. 
+@ContextConfiguration(locations={"file:src/main/webapp/WEB-INF/spring/**/*.xml"})
+public class MysqlTest {
+	@Autowired
+	private MysqlMapper mapper;
+	
+	@Test
+	public void getTime() { 
+		mapper.getTime(); 
+	}
+}
